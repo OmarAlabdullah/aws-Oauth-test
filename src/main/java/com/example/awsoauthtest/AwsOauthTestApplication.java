3f6c2bd1ad6e34416cac76d22a91698b9408a734
Omar Alabdullah <omar@xel.nl>
2022-09-30 13:00:15 +0200

    Initial commit

---
+++ src/main/java/com/example/awsoauthtest/AwsOauthTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsoauthtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsOauthTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsOauthTestApplication.class, args);
+    }
+
+}
